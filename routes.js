354d7afc5fa70fd483a99208f6bcb5368e8827b3
unknown <sanayasinghparihar@gmail.com>
2022-02-08 09:22:53 +0530

    Changes

--- routes.js
+++ routes.js
@@ -59,20 +59,18 @@ router.patch('/students/:id',async (req,res)=>{
 
 //delete api
 router.delete("/students/:name",async(req,res)=>{
-    const nam = req.params.name;
-    const out = await student.findByIdAndDelete(nam)
-    res.send(out)
+    await student.deleteOne({name:req.params.name},(err,msg)=>{
+        if(err){
+            res.status(500).json({
+                error:err
+            })
+        }
+        else{
+            res.status(200).json({
+                msg:msg
+            })
+        }
+    })
 })
-// await student.deleteOne({name:req.params.name},(err,msg)=>{
-    //     if(err){
-    //         res.status(500).json({
-    //             error:err
-    //         })
-    //     }
-    //     else{
-    //         res.status(200).json({
-    //             msg:msg
-    //         })
-    //     }
-    // })
+
 module.exports = router 
